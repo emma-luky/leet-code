aa3b2ecb2855910394f3fca6e45ea043fd9b01d9
Emma Luk <122421326+emma-luky@users.noreply.github.com>
2024-09-26 12:15:48 -0600

    Update BestTimeToBuyAndSellStock.cs

--- BestTimeToBuyAndSellStock.cs
+++ BestTimeToBuyAndSellStock.cs
@@ -1,24 +1,14 @@
 public class Solution {
     public int MaxProfit(int[] prices) {
-        int min = prices[0];
-        int index = 0;
-        for (int i = 1; i < prices.Length - 1; i++) {     
-            Console.WriteLine(i);   
-            if(prices[i] < min){
-                min = prices[i];
-                index = i;
+        int buy = prices[0];
+        int maxProfit = 0;
+        for (int i = 1; i < prices.Length; i++) {
+            if (prices[i] < buy) {
+                buy = prices[i];
+            } else if (prices[i] - buy > maxProfit) {
+                maxProfit = prices[i] - buy;
             }
         }
-
-        int max = 0;
-        for (int i = index + 1; i < prices.Length; i++){
-            if (prices[i] > max){
-                max = prices[i];
-            }
-        }
-        if (max - min < 0){
-            return 0;
-        }
-        return max - min;
+        return maxProfit;
     }
 }
