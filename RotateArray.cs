2ccd4896173d69c0b4b193a6bcdb49bb7a671a95
Emma Luk <122421326+emma-luky@users.noreply.github.com>
2024-09-26 12:43:31 -0600

    Update RotateArray.cs

--- RotateArray.cs
+++ RotateArray.cs
@@ -1,1 +1,23 @@
-// TODO
+public class Solution {
+    public void Rotate(int[] nums, int k) {
+        while (k > nums.Length){
+            k = k - nums.Length;
+        }
+        int[] rotatedArray = new int[nums.Length];
+        for (int i = 0; i < nums.Length; i++){
+            if(i < nums.Length - k){
+                rotatedArray[i + k] = nums[i];
+            }
+            else{
+                int index = i + k - nums.Length;
+                while (index > nums.Length){
+
+                }
+                rotatedArray[i + k - nums.Length] = nums[i];
+            }
+        }
+        for (int i = 0; i < nums.Length; i ++) {
+            nums[i] = rotatedArray[i];
+        }
+    }
+}
