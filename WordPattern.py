27cee82082f1aa5c5c5489dc5a2536fa939b491c
Emma Luk <122421326+emma-luky@users.noreply.github.com>
2025-01-14 11:25:28 -0700

    Optimized WordPattern.py

--- WordPattern.py
+++ WordPattern.py
@@ -8,15 +8,16 @@ class Solution(object):
         words = s.split()
         if len(pattern) != len(words):
             return False
-
+        usedWords = set()
         dictionary = {}
         for i in range(len(pattern)):
-            if words[i] in dictionary.values():
-                if pattern[i] not in dictionary.keys() or dictionary[pattern[i]] != words[i]:
+            if words[i] in usedWords:
+                if pattern[i] not in dictionary or dictionary[pattern[i]] != words[i]:
                     return False
             elif pattern[i] in dictionary:
                 if dictionary[pattern[i]] != words[i]:
                     return False
             else:
+                usedWords.add(words[i])
                 dictionary[pattern[i]] = words[i]
         return True
