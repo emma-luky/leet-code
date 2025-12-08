ad448fecb34b0128f3a16255e1ebe287255f6dcb
Emma Luk <122421326+emma-luky@users.noreply.github.com>
2025-12-08 14:40:50 -0800

    Create JumpGame.py

---
+++ JumpGame.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        energy = 0
+        for n in nums:
+            if energy < 0:
+                return False
+            elif n > energy:
+                energy = n
+            energy -= 1
+        return True
