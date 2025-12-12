0092b07a527d1c57c3887e45f62dfcac59ead1ea
Emma Luk <122421326+emma-luky@users.noreply.github.com>
2025-12-12 15:01:01 -0800

    Update MinimumSizeSubarraySum.py

--- MinimumSizeSubarraySum.py
+++ MinimumSizeSubarraySum.py
@@ -1,18 +1,21 @@
 class Solution(object):
     def minSubArrayLen(self, target, nums):
-        """
-        :type target: int
-        :type nums: List[int]
-        :rtype: int
-        """
-        nums.sort(reverse=True)
-        min_length = 0
-        for num in nums:
-            if(target <= 0):
-                break
-            target -= num
-            min_length += 1
-            print(target)
-        if(target > 0):
+        min_length = float('inf')
+        sum = 0
+        count = 0
+        l = 0
+        r = 0
+        while l < len(nums):
+            if sum < target and r < len(nums):
+                sum += nums[r]
+                r += 1
+                count += 1
+            else:
+                if sum >= target:
+                    min_length = min(min_length, count)
+                sum -= nums[l]
+                l += 1
+                count -= 1
+        if min_length == float('inf'):
             return 0
         return min_length
