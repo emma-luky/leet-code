62829c2bf7287d83879f42fe60bbf79526cf8332
Emma Luk <122421326+emma-luky@users.noreply.github.com>
2026-01-24 22:31:18 -0800

    Create FindTheIndexOfTheFirstOccuranceInAString.py

---
+++ Arrays-Strings/FindTheIndexOfTheFirstOccuranceInAString.py
@@ -0,0 +1,11 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle not in haystack:
+            return -1
+
+        # loop until the max index possible
+        for i in range(len(haystack) - len(needle) + 1):
+            if haystack[i:i + len(needle)] == needle:
+                return i
+        
+        return -1
