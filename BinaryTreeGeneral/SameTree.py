479375c99b65b5c18a7443451696a9ce4bc94d25
Emma Luk <122421326+emma-luky@users.noreply.github.com>
2026-04-01 23:35:14 -0700

    Create SameTree.py

---
+++ BinaryTreeGeneral/SameTree.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        node1 = p
+        node2 = q
+        if node1 is None and node2 is None:
+            return True
+        if node1 is None or node2 is None:
+            return False
+        if node1.val != node2.val:
+            return False
+        return self.isSameTree(node1.left, node2.left) and self.isSameTree(node1.right, node2.right)
