d88aa257ecd7399f81f7e1b5cfe792106e56b0b4
Emma Luk <122421326+emma-luky@users.noreply.github.com>
2026-04-29 22:59:01 -0700

    Create BinaryTreeZigZagLevelOrderTraversal.py

---
+++ BinaryTreeBFS/BinaryTreeZigZagLevelOrderTraversal.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root:
+            queue = deque([root])
+            LtoR = True
+            res = []
+            while queue:
+                size = len(queue)
+                level = []
+                for i in range(size):
+                    node = queue.popleft()
+                    level.append(node.val)
+                    if node.left:
+                        queue.append(node.left)
+                    if node.right:
+                        queue.append(node.right)
+                res.append(level if LtoR else level[::-1])
+                LtoR = not LtoR
+            return res
+        return []
